4317a701461172a783ea92bd25e77c773a1af46b
RonexLemon <ronexondimu@gmail.com>
2023-12-03 23:16:37 +0300

    init

--- src/Counter.sol
+++ src/Counter.sol
@@ -12,3 +12,6 @@ contract Counter {
         number++;
     }
 }
+// Deployer: 0x808476735803A20Cd00011f2271fC79E81b0F47F
+// Deployed to: 0x90678f0b196f01D0923029ad37b3824bE36e4963     ///this is the address 
+// Transaction hash: 0xece3343fa925ee71c655a911e80f55d0f6baa2f398525b9d41e1371d78ebebe5
